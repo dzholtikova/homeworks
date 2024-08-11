4a19c0c2a17225d1f8315249fe3ead02031588d2
Daria <dzholtikova@st.swps.edu.pl>
2024-08-11 12:35:29 +0300

    Added new functions

--- homework5-2.js
+++ homework5-2.js
@@ -1,19 +1,44 @@
 var services = {
   стрижка: "60 грн",
   гоління: "80 грн",
-  миттяГолови: "100 грн",
-
-  calculateTotalPrice: function () {
-    var totalPrice = 0;
-    for (var key in this) {
-      if (typeof this[key] === "string" && this[key].endsWith(" грн")) {
-        var price = parseInt(this[key]);
-        totalPrice += price;
-      }
-    }
-    return totalPrice + " грн";
-  },
+  "Миття голови": "100 грн",
 };
 
-var totalPrice = services.calculateTotalPrice();
-console.log(totalPrice);
+function totalPrice() {
+  var total = 0;
+  for (var key in services) {
+    var price = parseInt(services[key]);
+    total += price;
+  }
+  return total + " грн";
+}
+
+function minPrice() {
+  var min = null;
+  for (var key in services) {
+    var price = parseInt(services[key]);
+    if (min === null) {
+      min = price;
+    } else if (price < min) {
+      min = price;
+    }
+  }
+  return min + " грн";
+}
+
+function maxPrice() {
+  var max = null;
+  for (var key in services) {
+    var price = parseInt(services[key]);
+    if (max === null) {
+      max = price;
+    } else if (price > max) {
+      max = price;
+    }
+  }
+  return max + " грн";
+}
+
+console.log("Загальна вартість: " + totalPrice());
+console.log("Мінімальна вартість: " + minPrice());
+console.log("Максимальна вартість: " + maxPrice());
